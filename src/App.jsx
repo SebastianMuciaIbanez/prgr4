709f8e921865d1bce867483e03044c83ca6c452b
Sebastian Ibañez <sebas-ibanes@hotmail.com>
2023-10-21 09:19:00 -0500

    primeros usos de elementos y objetos con react

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import leon from "../leon.jpg"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const elementOne = <h1>Hello, world! one</h1>;
+const name = 'Josh Perez';
+const element = <h1>Hello, {`2+2= ${2 + 2} ${name}`}</h1>;
+const objExample = {
+  name: 'John', 
+  lastName: 'Villegas', 
+  age: 30, 
+  city: 'New York'
+};
 
+const tabIndexAux = 3;
+
+function formatName(User){
+  return User.name + " " + User.lastName;
+}
+
+const elementFullName = (
+<div>
+  <h1>Hola, nombre completo: {formatName(objExample)}</h1>
+  <h1>Hola, nombre completo: {formatName(objExample)}</h1>
+</div>
+) 
+
+function renderAge(user) {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>La edad es de: {user.age}</h1>
+      <h1>La edad es de: {user.age}</h1>
     </>
+  )
+}
+
+function App() {
+  //const [count, setCount] = useState(0)
+  const elementTwo = (
+    <h1 id="id-unico" tabIndex={tabIndexAux} className='Hola'>
+      Hello, world! two
+    </h1>
+  );
+  return (
+    <div>
+      {elementOne}
+      {elementTwo}
+      {element}
+      <p style={{
+        border: 'red solid',
+        borderRadius: '18px',
+        backgroundColor: 'yellow'
+      }}
+      >
+        El nombre completo es: {formatName(objExample)}</p>
+      {elementFullName}
+      {renderAge(objExample)}
+      <img src={leon} alt="react-logo"/>
+    </div>
   )
 }
 
